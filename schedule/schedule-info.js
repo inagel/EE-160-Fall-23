6083a014b19c85cda13f391f1ed15e03cddc7b92
inagel <inagel@users.noreply.github.com>
2023-11-21 12:22:01 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,5 @@
+moreaEventData = [
+  {title: "Visual Studio Code", url: "/EE-160-Test/modules/vsc", start: "2023-08-21", end: "2023-12-15T23:00"},
+  {title: "A01: Assess your ability to use the basic functions within Visual Code Studio.", url: "/EE-160-Test/morea/EE_160_VSC/assessment-vsc.html", start: "2023-08-21T09:00"},
+  {title: "E01: What are the basic features of VS Code?", url: "/EE-160-Test/morea/EE_160_VSC/experience-vsc.html", start: "2023-08-21T23:00"}
+]
